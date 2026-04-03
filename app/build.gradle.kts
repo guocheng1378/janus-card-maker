7c26474e1850bb1d2b980be6772fb1beef36b7d2
guocheng <guocheng@example.com>
2026-04-04 03:43:09 +0800

    ci: 构建 debug APK（暂不需要签名）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,26 +15,9 @@ android {
         versionName = "3.0.0"
     }
 
-    signingConfigs {
-        create("release") {
-            storeFile = file("janus-release.p12")
-            storePassword = "januscardmaker"
-            keyAlias = "janus"
-            keyPassword = "januscardmaker"
-        }
-        // CI: 从环境变量读取
-        create("ci") {
-            storeFile = file("${System.getenv("KEYSTORE_PATH")}")
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = "janus"
-            keyPassword = System.getenv("KEY_PASSWORD")
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("ci")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
